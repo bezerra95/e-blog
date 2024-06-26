78d60d979f35802aad022c0c106735c0e95a1efe
bezerra95 <jcbs3095@gmail.com>
2024-06-25 22:07:58 -0300

    Add User model

---
+++ src/main/kotlin/com/eprogramar/blog/IndexController.kt
@@ -0,0 +1,17 @@
+package com.eprogramar.blog
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+
+@Controller
+@RequestMapping("/")
+class IndexController {
+
+    @GetMapping
+    fun index(): String {
+        return "index"
+    }
+}
---
+++ src/main/kotlin/com/eprogramar/blog/User.kt
@@ -0,0 +1,7 @@
+package com.eprogramar.blog
+
+data class User(
+    val name: String,
+    val email: String,
+    val password: String
+)
