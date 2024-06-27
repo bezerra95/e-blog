85275f8180fd79f095836a9d890f74d0994aae58
bezerra95 <jcbs3095@gmail.com>
2024-06-26 23:04:35 -0300

    Add logger

--- src/main/kotlin/com/eprogramar/blog/IndexController.kt
+++ src/main/kotlin/com/eprogramar/blog/IndexController.kt
@@ -1,5 +1,7 @@
 package com.eprogramar.blog
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/")
 class IndexController {
 
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
     @GetMapping
     fun index(): String {
+        logger.info("index()...")
         return "index"
     }
 }
--- src/main/kotlin/com/eprogramar/blog/User.kt
+++ src/main/kotlin/com/eprogramar/blog/User.kt
@@ -1,7 +1,16 @@
 package com.eprogramar.blog
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
 data class User(
-    val name: String,
-    val email: String,
-    val password: String
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    var name: String = "",
+    var email: String = "",
+    var password: String = ""
 )
