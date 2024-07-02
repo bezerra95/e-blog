9450bcad3bb75f1365a8ee33b2d4d790ba7718b5
bezerra95 <jcbs3095@gmail.com>
2024-07-02 12:07:14 -0300

    Add login controller model

--- src/main/kotlin/com/eprogramar/blog/LoginController.kt
+++ src/main/kotlin/com/eprogramar/blog/LoginController.kt
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 
 @Controller
 @RequestMapping("/login")
-class LoginController {
+class LoginController(
+    private val repository: UserRepository
+) {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
@@ -19,5 +22,28 @@ class LoginController {
         logger.info("form()...")
         model.addAttribute("user", User())
         return "login"
+    }
+
+    @PostMapping
+    fun login(user: User, model: Model): String {
+        logger.info("Login($user)")
+
+        val optional = repository.findByEmail(user.email)
+        if (optional.isEmpty) {
+            val messageError = "Usuário não localizado"
+            logger.error(messageError)
+            model.addAttribute("messageError", messageError)
+            return "login"
+        }
+
+        val userDatabase = optional.get()
+        if (user.password != userDatabase.password) {
+            val messageError = "Senha inválida!"
+            logger.error(messageError)
+            model.addAttribute("messageError", messageError)
+            return "login"
+        }
+        logger.info("Login executado com sucesso")
+        return "redirect:/"
     }
 }
