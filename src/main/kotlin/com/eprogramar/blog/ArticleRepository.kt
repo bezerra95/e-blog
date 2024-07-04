c7988ba6b7720dadc1651f3dc308fd3b9cea2e4b
bezerra95 <jcbs3095@gmail.com>
2024-07-04 19:34:29 -0300

    Add Article template

--- src/main/kotlin/com/eprogramar/blog/ArticleRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.eprogramar.blog
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ArticleRepository: JpaRepository<Article, Long>
